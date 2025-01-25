1c15593d75328ebe53c66ccf869d5d8c203b1bbf
watchdog92 <cristhian.pt.1992@gmail.com>
2025-01-25 08:27:09 -0500

    Alumvida terminado

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("Este mensaje aparece en la consola del navegador.");
+window.alert("Este mensaje aparece en una caja de alerta.");
+document.getElementById("output").innerHTML = "Este mensaje aparece dentro del elemento HTML con id 'output'.";
+document.write("Este mensaje aparece directamente en la salida estándar del documento HTML.");
